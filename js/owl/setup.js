254584efe4bf8e228bb132ccea2d1fbd86ee76d2
Rafael Mota <fael.folow@gmail.com>
2022-07-30 21:45:59 -0300

    Adiciona footer e alterações no carousel

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -2,9 +2,14 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    dotsEach:true,
+    autoplay:true,
+    autoplayTimeout: 3000, // Velocidade do autoplay
+    autoplayHoverPause:true,
+    smartSpeed:2500, // Velocidade da transição de imagem
     responsive:{
         0:{
-            items:1
+            items:3
         },
         600:{
             items:3
